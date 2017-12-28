29c2cc89c27894a1ef5ca39b020f9bc49d4fdc29
MILAPATEL18\Milap Patel <milapatel18@gmail.com>
2017-12-28 19:58:30 +0530

    SAMPLE WITH JAVA - FXML

---
+++ FirstAttempt/src/fxmlfirst/MyApplication.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fxmlfirst;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Read file fxml and draw interface.
+            Parent root = FXMLLoader.load(getClass().getResource("MyScene.fxml"));
+
+            primaryStage.setTitle("My Application");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
